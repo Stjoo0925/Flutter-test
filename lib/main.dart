8abe581338d06e4b8fd4a98cfcd9a0575f8ee620
Stjoo0925 <stjoo0925@gmail.com>
2024-08-18 23:47:55 +0900

    feat : add decoration

--- lib/main.dart
+++ lib/main.dart
@@ -15,8 +15,24 @@ class MyApp extends StatelessWidget {
           backgroundColor: Colors.blue,
           title: Text('연락처'),
         ),
-        body: Center(
-          child: Text('안녕')
+        body: Align(
+          alignment: Alignment.center,
+          child: Container(
+            width: double.infinity,
+            height: 50,
+            decoration: BoxDecoration(
+              color: Colors.blueAccent,
+              border: Border.all(color: Colors.black),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black, // 그림자의 색상
+                  offset: Offset(2, 2), // 그림자의 위치 (x, y)
+                  blurRadius: 4.0, // 블러 효과의 반경
+                  spreadRadius: 1.0, // 그림자가 퍼지는 정도
+                ),
+              ],
+            ),
+          ),
         ),
         bottomNavigationBar: BottomAppBar(
           color: Colors.blue,
